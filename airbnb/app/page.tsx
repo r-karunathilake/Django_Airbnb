ed642c5bde2d9f8743e0d2c72635748f26660cea
Ravindu Karunathilake <karunath@ualberta.ca>
2024-06-17 13:48:39 -0600

    Created initial project template for the airbnb clone.

---
+++ airbnb/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      Django Airbnb 
+      <h1 className="text-airbnb">This is Airbnb main color</h1>
+      <h1 className="text-airbnb-dark">This is Airbnb main dark color</h1>
+    </main>
+  );
+}

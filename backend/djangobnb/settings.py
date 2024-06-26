0b9068f55656efa08a6089cec82290795664264f
Ravindu Karunathilake <karunath@ualberta.ca>
2024-06-26 17:40:05 -0600

    Created a custom user model in Django.

--- backend/djangobnb/settings.py
+++ backend/djangobnb/settings.py
@@ -15,6 +15,8 @@ DEBUG = bool(os.environ.get("DEBUG", default=0))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split()
 
+AUTH_USER_MODEL = 'useraccount.User'
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -24,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'useraccount',
 ]
 
 MIDDLEWARE = [
--- backend/useraccount/models.py
+++ backend/useraccount/models.py
@@ -1,3 +1,50 @@
+import uuid 
+
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+from django.conf import settings 
 from django.db import models
 
-# Create your models here.
+class CustomUserManager(UserManager):
+    """
+    Custom user model manager where email is the unique identifier for 
+    authentication instead of usernames. 
+    """
+    def _create_user(self, name, email, password, **extra_fields):
+        if not email:
+            raise ValueError("Please specify a valid e-mail address")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+        
+        return user
+
+    def create_user(self, name, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(name, email, password, **extra_fields)
+
+    def create_superuser(self, name, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self._create_user(name, email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    avatar = models.ImageField(upload_to="uploads/avatars")
+
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(blank=True, null=True)
+
+    objects = CustomUserManager() 
+
+    USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = ["name",]

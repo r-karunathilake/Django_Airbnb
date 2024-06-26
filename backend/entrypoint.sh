3ec08f829a7e957acb8ad626199d1e6a8c04a826
Ravindu Karunathilake <karunath@ualberta.ca>
2024-06-26 13:54:32 -0600

    Setup docker for Django with PostgresSQL.

---
+++ backend/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh 
+
+if ["$DATABASE" = "postgres"]; then
+    echo "Checking if database is running..." 
+
+    while !nc -z $SQL_HOST $SQL_PORT; do
+        sleep 0.1
+    done 
+
+    echo "The database is up and running: :)" 
+fi 
+
+python manage.py migrate 
+
+exec "$@"

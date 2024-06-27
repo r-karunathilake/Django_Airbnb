875a3212530c838df3a537545b57f82ae93094fd
Ravindu Karunathilake <karunath@ualberta.ca>
2024-06-26 20:50:43 -0600

    Connected the front-end and back-end to get and display property listing.

--- airbnb/app/components/properties/property.tsx
+++ airbnb/app/components/properties/property.tsx
@@ -1,24 +1,29 @@
 import Image from "next/image"
+import { PropertyType } from "./property_listing";
 
-const PropertyListItem = () =>{
+interface PropertyProps{
+    property: PropertyType;
+}
+
+const PropertyListItem: React.FC<PropertyProps> = ({property}) =>{
     return (
         <div className="cursor-pointer">
             <div className="relative overflow-hidden aspect-square rounded-xl">
                 <Image 
                     fill
-                    src="/properties/national_parks.jpeg"
+                    src={property.image_url}
                     sizes="(max-width: 768px) 768px, (max-width: 1200px): 768px, 768px"
                     className="hover:scale-110 object-cover transition h-full w-full"
                     alt="National Park Cabin"
                 />
             </div>
             <div className="mt-2">
-                <p className="text-lg font-bold">Property Name</p>
+                <p className="text-lg font-bold">{property.title}</p>
             </div>
 
             <div className="mt-2">
                 <p className="text-sm text-gray-700">
-                    $200<strong> per night</strong>
+                    ${property.price_per_night}<strong> per night</strong>
                 </p>
                 
             </div>
